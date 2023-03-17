82e74682809abc2c602a702e5e46c23e67de78a0
trantung <trantunghn196@gmail.com>
2023-03-17 22:11:13 +0700

    apidoc update

--- apidocument/api.php
+++ apidocument/api.php
@@ -599,6 +599,101 @@
  */
 
 /**
+ * @api {get} /api/home/class/list Danh sách lớp
+ * @apiName HomeGetClassList
+ * @apiGroup Frontend
+ * @apiSuccess {Integer} status Status of response
+ * @apiSuccess {String} data  data response
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+*{
+*    "success": 1,
+*    "message": null,
+*    "code": 0,
+*    "data": [
+*        {
+*            "id": 1,
+*            "name": "Lớp 1"
+*        },
+*        {
+*            "id": 2,
+*            "name": "Lớp 2"
+*        },
+*        {
+*            "id": 3,
+*            "name": "Lớp 3"
+*        },
+*        {
+*            "id": 4,
+*            "name": "Lớp 4"
+*        },
+*        {
+*            "id": 5,
+*            "name": "Lớp 5"
+*        },
+*        {
+*            "id": 6,
+*            "name": "Lớp 6"
+*        },
+*        {
+*            "id": 7,
+*            "name": "Lớp 8"
+*        },
+*        {
+*            "id": 8,
+*            "name": "Lớp 7"
+*        },
+*        {
+*            "id": 9,
+*            "name": "Lớp 9"
+*        },
+*        {
+*            "id": 10,
+*            "name": "Lớp 10"
+*        },
+*        {
+*            "id": 11,
+*            "name": "Lớp 11"
+*        },
+*        {
+*            "id": 12,
+*            "name": "Lớp 12"
+*        }
+*    ]
+*}
+ */
+/**
+ * @api {get} /api/home/subject/list Danh sách môn
+ * @apiName HomeGetSubjectList
+ * @apiGroup Frontend
+ * @apiSuccess {Integer} status Status of response
+ * @apiSuccess {String} data  data response
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+* {
+*     "success": 1,
+*     "message": null,
+*     "code": 0,
+*     "data": [
+*         {
+*             "id": 1,
+*             "name": "Toán"
+*         },
+*         {
+*             "id": 2,
+*             "name": "Lý"
+*         },
+*         {
+*             "id": 3,
+*             "name": "Hóa"
+*         }
+*     ]
+* }
+ */
+
+/**
  * @api {post} /api/cms/course/list Danh sách course cho cms
  * @apiName CmsCourseList
  * @apiGroup CMS
